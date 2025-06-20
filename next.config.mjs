75c4049fd1b1cb34d61100d16031c9a01d9ac1a7
Sachi8565 <101034229+Sachi8565@users.noreply.github.com>
2025-06-20 09:52:05 +0530

    301 redirection .php

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/:path*.php',   // matches all old PHP URLs
+        destination: '/:path*',  // redirects to new URL without .php
+        permanent: true,         // 301 redirect
+      },
+    ];
+  },
+};
 
 export default nextConfig;
